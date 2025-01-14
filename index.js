7be345050d94aa797cc274a24d969fd6f4fa1f4e
Sandesh Bhattarai <sandesh.bhattarai79@gmail.com>
2025-01-14 17:07:39 +0545

    Updated for Koyeb to be updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require("./src/config/express.config")
 
 const httpServer = http.createServer(app)
 
-httpServer.listen(9005, '192.168.101.191', (e) => {
+httpServer.listen(process.env.PORT || 80, (e) => {
     if(!e) {
         console.log("Server is running on port ",9005)
         console.log("Press CTRL+C to discontinue server...")
